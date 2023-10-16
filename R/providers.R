add3e7a47d7ed0cfeb1427bd4ca0a6183b3083cb
Palate Jean <jean.palate@nbb.be>
2023-10-16 12:20:39 +0200

    Pdfs...

--- R/providers.R
+++ R/providers.R
@@ -4,5 +4,22 @@ print(rjd3providers::spreadsheet_content("belgium.xlsx"))
 indprod<-rjd3providers::spreadsheet_data('belgium.xlsx', 1)
 plot(indprod$series$`Manufacture of textiles`$data, col='blue')
 
-err<-lapply(indprod$series, function(z)rjd3tramoseats::terror(z$data, 'tr1', nback=6))
+errs<-lapply(indprod$series, function(z)rjd3tramoseats::terror(z$data, 'tr1', nback=24))
+
+
+err<-errs$`Manufacture of food products`
+
+library(ggplot2)
+
+w<-data.frame(date=rjd3toolkit::daysOf(err[,1]), y=as.numeric(err[,5]),
+              fcasts=as.numeric(err[,6]),
+              min=as.numeric(err[,6]-2*err[,7]),
+              max=as.numeric(err[,6]+2*err[,7]))
+
+p<-ggplot(data=w) +
+  geom_line(aes(x=date, y=fcasts), color='blue') +
+  geom_line(aes(x=date, y=y), color='red') +
+  geom_ribbon(aes(x=date, ymin=min, ymax=max), alpha=.2)
+print(p)
+
 

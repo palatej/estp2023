2291f7febf208f655220c8c77c3c6d9691c2fd4b
Palate Jean <jean.palate@nbb.be>
2023-10-18 09:30:04 +0200

    SEATS, seasonality tests

---
+++ R/confidence.R
@@ -0,0 +1,41 @@
+rjd3providers::set_spreadsheet_paths('./Data')
+#print(rjd3providers::spreadsheet_content("belgium.xlsx"))
+xbe<-rjd3providers::spreadsheet_data('belgium.xlsx', 2)
+
+s<-xbe$series$France$data
+#s<-xbe$series$Netherlands$data
+
+#s<-rjd3toolkit::retail$DepartmentStoresExclDiscountDepa
+rslt<-rjd3tramoseats::tramoseats_fast(s, 'rsafull')
+sa<-rslt$final$sa$data
+csa<-rslt$decomposition$stochastics$sa$data
+esa<-rslt$decomposition$stochastics$sa$data.stde
+
+if (rslt$preprocessing$description$log){
+  v <- esa*esa
+  esa<-exp(csa + 0.5*v)*sqrt(exp(v)-1)
+}
+
+xrslt<-rjd3x13::x13_fast(s, 'rsa5')
+xsa<-xrslt$final$d11final
+
+library(ggplot2)
+
+#number of points
+n<-60
+#number of stdev
+m<-2
+w<-data.frame(date=rjd3toolkit::daysOf(s)[1:n],
+              y=as.numeric(s)[1:n],
+              sa=as.numeric(sa)[1:n],
+              xsa=as.numeric(xsa)[1:n],
+              min=as.numeric(sa-m*esa)[1:n],
+              max=as.numeric(sa+m*esa)[1:n])
+
+p<-ggplot(data=w) +
+  geom_line(aes(x=date, y=y), color='black') +
+  geom_line(aes(x=date, y=sa), color='red') +
+  geom_line(aes(x=date, y=xsa), color='green') +
+  geom_ribbon(aes(x=date, ymin=min, ymax=max), alpha=.2)
+print(p)
+
---
+++ R/revisions.R
@@ -0,0 +1,58 @@
+s<-rjd3toolkit::retail$BookStores
+policy<-"Fixed"
+
+sa<-rjd3tramoseats::tramoseats(s, "rsa0")
+nspec<-rjd3tramoseats::tramoseats_refresh(sa$result_spec, sa$estimation_spec,policy)
+
+ts_ids<-list(list(period="2006-01-01", start="2006-01-01", id="sa"), list(period="2006-01-01", start="2006-01-01", id="i"))
+rh<-rjd3tramoseats::tramoseats_revisions(s, nspec, ts_ids = ts_ids)
+
+plot(rh$series$sa, type='l')
+
+# or, equivalently in this case
+nspec2<-rjd3tramoseats::tramoseats_refresh(sa$result_spec, sa$estimation_spec, policy)
+# this is actually what is done in the graphical interface
+rh2<-rjd3tramoseats::tramoseats_revisions(s, nspec2, ts_ids = ts_ids)
+
+# same result
+summary(rh$series$i-rh2$series$i)
+
+cmp_ids<-list(list(start="2006-01-01", end="2010-12-31", id="sa"), list(start="2006-01-01", end="2010-12-31", id="t"))
+rh3<-rjd3tramoseats::tramoseats_revisions(s, nspec, ts_ids =  ts_ids,cmp_ids =  cmp_ids)
+
+
+csa<-rh3$components$sa
+d<-dim(csa)
+#we compare each vintage with the last one. Up to you to choose the way you compare them
+rcsa_ts<-100*(csa/csa[,d[2]]-1)
+
+plot(rcsa_ts[,1])
+
+#comparison of the first and last estimations (what is done in the graphical interface)
+diff<-diag(rcsa_ts[(d[1]-d[2]+1):d[1],])
+diff<-diff[-length(diff)]
+print(diff)
+summary(diff)
+
+# same with x13
+
+sa<-rjd3x13::x13(s, "rsa0")
+nspec<-rjd3x13::x13_refresh(sa$result_spec, sa$estimation_spec, policy)
+
+ts_ids<-list(list(period="2006-01-01", start="2006-01-01", id="sa"), list(period="2006-01-01", start="2006-01-01", id="i"))
+rh<-rjd3x13::x13_revisions(s, nspec, ts_ids = ts_ids)
+
+plot(rh$series$sa, type='l')
+
+cmp_ids<-list(list(start="2006-01-01", end="2010-12-31", id="sa"), list(start="2006-01-01", end="2010-12-31", id="t"))
+rh3<-rjd3x13::x13_revisions(s, nspec, NULL, ts_ids, cmp_ids)
+
+
+csa<-rh3$components$sa
+d<-dim(csa)
+#we compare each vintage with the last one. Up to you to choose the way you compare them
+rcsa_x13<-100*(csa/csa[,d[2]]-1)
+
+plot(rcsa_x13[,1])
+lines(rcsa_ts[,1], col='red')
+

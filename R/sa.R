bc03a91920594c66dd3e479adac6af0b35f00cb9
Palate Jean <jean.palate@nbb.be>
2023-10-19 10:53:28 +0200

    sa example

--- R/sa.R
+++ R/sa.R
@@ -4,12 +4,17 @@ spec<-rjd3tramoseats::tramoseats_spec("rsafull")
 spec$tramo$regression$td$auto<-"AUTO_BIC"
 sa_tramoseats<-rjd3tramoseats::tramoseats_fast(s, spec)
 
-spec<-rjd3x13::x13_spec("rsa4")
-spec$regarima$regression$td$auto<-"AUTO_BIC"
-sa_x13<-rjd3x13::x13_fast(s, spec)
+xspec<-rjd3x13::x13_spec("rsa4")
+xspec$regarima$regression$td$auto<-"AUTO_BIC"
+sa_x13<-rjd3x13::x13_fast(s, xspec)
 
 print(sa_tramoseats$preprocessing)
 print(sa_x13$preprocessing)
 
 plot(sa_tramoseats)
 plot(sa_x13)
+
+sa_x13c<-rjd3x13::x13_fast(s, xspec, userdefined = c('decomposition.b7', 'decomposition.c7'))
+
+plot(sa_x13$decomposition$d9, type='h')
+
